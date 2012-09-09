aa81780c890c9b44fcbe056bebdb0ba11af7f034
voodeng <voodeng@gmail.com>
2012-09-09 17:31:18 +0800

    +devise and cancan

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todev::Application.config.secret_token = '99fd8c307b0425900668a0a83db8691ec25f9eb2f9f574563073ab5afddb221abb34080055e97665e280672e2edb4fcac5067a34ce943356a1164991852972a7'
